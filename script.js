cfa1e1c9df844b21277ee8fa74657038991f7559
lilvlad-4life <vladzagrebelnyi122@gmail.com>
2021-07-06 18:41:17 +0300

    finish animation

--- script.js
+++ script.js
@@ -9,21 +9,88 @@ const playerImage = new Image();
 playerImage.src = "./image/shadow_dog.png";
 const spriteWidth = 575;
 const spriteHeight = 532;
-let frameX = 0;
-let frameY = 0;
+let playerState = "idle";
+
+const dropdown = document.querySelector("#animations");
+
+dropdown.addEventListener("change", (e) => {
+  playerState = e.target.value;
+});
+
 let gameFrame = 0;
 const staggerFrames = 5;
 
+let spriteAnimations = [];
+let animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+  {
+    name: "fall",
+    frames: 7,
+  },
+  {
+    name: "run",
+    frames: 9,
+  },
+  {
+    name: "dizzy",
+    frames: 11,
+  },
+  {
+    name: "sit",
+    frames: 5,
+  },
+  {
+    name: "roll",
+    frames: 7,
+  },
+  {
+    name: "bite",
+    frames: 7,
+  },
+  {
+    name: "ko",
+    frames: 12,
+  },
+  {
+    name: "getHit",
+    frames: 7,
+  },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+
+  spriteAnimations[state.name] = frames;
+});
+
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_WIDTH);
-  let position = Math.floor(gameFrame / staggerFrames) % 6;
-  
-  frameX = spriteWidth * position;
-  
+  let position =
+    Math.floor(gameFrame / staggerFrames) %
+    spriteAnimations[playerState].loc.length;
+
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations[playerState].loc[position].y;
+
   ctx.drawImage(
     playerImage,
     frameX,
-    frameY * spriteHeight,
+    frameY,
     spriteWidth,
     spriteHeight,
     0,
